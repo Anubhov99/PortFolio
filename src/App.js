d5ecd86359c3117f3e7c90c1fc2532db42ad8d79
Anubhov99 <m.anubhov99@gmail.com>
2024-01-20 23:09:47 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import photo from './passpic.jpg'
+let name = "Anubhov Menon"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </nav>
+    <br />
+    <br />
+    <br />
+    <div>
+    <img src={photo} className = "App-logo" alt="Anubhov Menon" />
     </div>
+
+    <br></br>
+    
+    <div className="container">
+      <h1>Hello this is {name}</h1>
+      <h2>About me</h2>
+      Hola!!
+      I am a software developer. I have 3 years of IT experience.
+      I have done numerous projects in the past 3 years mostly in the financial services domain.
+      This web page is also developed by me! Hover on the sections above to know more about me.
+      
+    </div>
+   
+    
+  
+
+    
+    </>
   );
+ 
 }
 
 export default App;
